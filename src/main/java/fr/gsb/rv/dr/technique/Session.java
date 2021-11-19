ce2679c1d78da5a1618751266b53ac6f5048b1c3
AntoineTSIO <antoine.touzart.sio@gmail.com>
2021-11-19 14:57:12 +0100

    V0.2.2

--- src/main/java/fr/gsb/rv/dr/technique/Session.java
+++ src/main/java/fr/gsb/rv/dr/technique/Session.java
@@ -1,10 +1,32 @@
 package fr.gsb.rv.dr.technique;
 
+import fr.gsb.rv.dr.entites.Visiteur;
+
 public class Session {
 
-    private Session session = null ;
+    private static Session session = null ;
 
+    public Session(Session session) {
+        this.session = session;
+    }
 
+    public static Session getSession() {
+        return session;
+    }
 
+    public void setSession(Session session) {
+        this.session = session;
+    }
 
+    public static void ouvrir(Visiteur visiteur){
+
+    }
+
+    public static void fermer(){
+
+    }
+
+    public static boolean estOuverte(){
+        if(session == )
+    }
 }
