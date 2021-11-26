04aae1f863aa4d2a078a65355a1ebf5654efce83
AntoineTSIO <antoine.touzart.sio@gmail.com>
2021-11-26 14:55:43 +0100

    V0.6.4

--- src/main/java/fr/gsb/rv/dr/gsbrvdr/PanneauAccueil.java
+++ src/main/java/fr/gsb/rv/dr/gsbrvdr/PanneauAccueil.java
@@ -1,5 +1,6 @@
 package fr.gsb.rv.dr.gsbrvdr;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -13,14 +14,10 @@ public class PanneauAccueil extends StackPane {
     public PanneauAccueil() {
         super();
         VBox root = new VBox();
-        Label label = new Label("Accueil");
         Image image = new Image("https://i.imgur.com/k1xhSgD.png");
         ImageView imageView = new ImageView(image);
 
-        label.setAlignment(Pos.CENTER);
-        label.setTextAlignment(TextAlignment.CENTER);
         root.setStyle("-fx-background-color: white ; -fx-alignment: center");
-        root.getChildren().add(label);
         root.getChildren().add(imageView);
         this.getChildren().add(root);
     }
