bd3c5827ce8b6e3ebc2984f021479157d40fb60c
AntoineTSIO <antoine.touzart.sio@gmail.com>
2021-11-29 11:35:41 +0100

    V0.8.4

--- src/main/java/fr/gsb/rv/dr/panneaux/PanneauPraticiens.java
+++ src/main/java/fr/gsb/rv/dr/panneaux/PanneauPraticiens.java
@@ -3,15 +3,23 @@ package fr.gsb.rv.dr.panneaux;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
-import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
 public class PanneauPraticiens extends StackPane {
+
+    protected static int CRITERE_COEF_CONFIANCE = 1 ;
+    protected static int CRITERE_COEF_NOTORIETE = 2 ;
+    protected static int CRITERE_DATE_VISITE = 3 ;
+    private int critereTri = CRITERE_COEF_CONFIANCE ;
+    private RadioButton rbCoefConfiance ;
+    private RadioButton rbCoefNotoriete ;
+    private RadioButton rbDateVisite ;
 
     public PanneauPraticiens(){
         super();
@@ -33,4 +41,15 @@ public class PanneauPraticiens extends StackPane {
         this.getChildren().add(root);
     }
 
+    public void rafraichir(){
+
+    }
+
+    public int getCritereTri() {
+        return critereTri;
+    }
+
+    public void setCritereTri(int critereTri) {
+        this.critereTri = critereTri;
+    }
 }
